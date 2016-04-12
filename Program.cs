1a6ed5ed92bd61c82d31c1e158cc92b3a98a33cd
ccampos <ccampos@simplifydigital.co.uk>
2016-04-12 17:47:53 +0100

    inital code - runs with localhost IIS

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.Web.Administration;
+
+namespace IIS_Bindings_Scrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // http://stackoverflow.com/questions/22458410/credentials-for-servermanager-openremote
+            //using (var mgr = ServerManager.OpenRemote("sim-dev-web"))
+            using (var mgr = new ServerManager())
+            {
+                foreach (var site in mgr.Sites)
+                {
+                    var name = site.Name;
+                    var state = site.State;
+                    var bindings = site.Bindings;
+
+                    Console.WriteLine("==================");
+                    Console.WriteLine("New Site:");
+                    Console.WriteLine("   Name:  '{0}'", name);
+                    Console.WriteLine("   State: '{0}'", state);
+                    Console.WriteLine("   Bindings: ");
+                    foreach (var binding in bindings)
+                    {
+                        //Console.WriteLine("      {0}", binding.EndPoint);
+                        //Console.WriteLine("      {0}", binding.Host);
+                        //Console.WriteLine("      {0}", binding.Protocol);
+                        Console.WriteLine("      {0}", binding.BindingInformation);
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SiteDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace IIS_Bindings_Scrapper
+{
+    class SiteDto
+    {
+        public string Name;
+        public string State;
+        public List<string> Bindings;
+
+    }
+}
